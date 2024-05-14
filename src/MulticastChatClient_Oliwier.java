4246867497646f2092eed6088710aab7edd84d02
OLIWIERTE21C <oliwier.kondratowicz@elev.ga.ntig.se>
2024-05-14 14:32:46 +0200

    fixade

--- src/MulticastChatClient_Oliwier.java
+++ src/MulticastChatClient_Oliwier.java
@@ -4,17 +4,19 @@ import java.io.*;
 public class MulticastChatClient_Oliwier {
     public static void main(String[] args)
             throws Exception {
+
+
                 int portnumber = 4444;
                 if (args.length >= 1){
                     portnumber = Integer.parseInt(args[0]);
                 }
-                    MulticastSocket chatMulticastSocket = new MulticastSocket(4444);
+                    MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
 
-                    InetAddress group = InetAddress.getByName("225.4.5.6");
+                    InetAddress group = InetAddress.getByName("225.2.5.6");
 
                     chatMulticastSocket.joinGroup(group);
 
-                    String msg = "";
+                    String msg = " ";
                     System.out.println("Type a message for the server:");
                     BufferedReader br=
                             new BufferedReader(new InputStreamReader(System.in));
