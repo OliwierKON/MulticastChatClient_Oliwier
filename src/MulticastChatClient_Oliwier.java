9f7824992e2bc144f078f7f603048990db6bd670
OLIWIERTE21C <oliwier.kondratowicz@elev.ga.ntig.se>
2024-05-14 14:22:13 +0200

    SKAPADE

---
+++ src/MulticastChatClient_Oliwier.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+public class MulticastChatClient_Oliwier {
+    public static void main(String[] args)
+            throws Exception {
+                int portnumber = 4444;
+                if (args.length >= 1){
+
+                    MulticastSocket chatMulticastSocket = new MulticastSocket(4444);
+
+                    InetAddress group = InetAddress.getByName("225.4.5.6");
+
+                    chatMulticastSocket.joinGroup(group);
+
+                    String msg = " ";
+                    System.out.println("Type a message for the server:");
+                    BufferedReader br=
+                            new BufferedReader(new InputStreamReader(System.in));
+                    msg = br.readLine();
+
+                    DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+                    chatMulticastSocket.send(data);
+
+                    chatMulticastSocket.close();
+                }
+
+        }
+
+
+    }
